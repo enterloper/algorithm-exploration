15721e891d0bc50cf8bb759f895d3b4c13dd12ba
Rich Boothe <richardjboothe@gmail.com>
2020-11-13 00:38:22 -0600

    update Remove Vowels From String

---
+++ LeetCode/Arrays_and_Strings/Remove_Vowels_From_String/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+const removeVowels = str => {
+    const vowels = {
+        a: true,
+        e: true,
+        i: true,
+        o: true,
+        u: true
+    };
+    let output = '';
+    for(let i = 0, l = str.length; i < l; i++) {
+        if (!vowels[str[i]]) {
+            output += str[i];
+        }
+    }
+    return output;
+};
