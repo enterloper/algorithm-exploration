59811cdc15e15fda524ef5496674cfa4437b9917
Rich Boothe <richardjboothe@gmail.com>
2020-11-28 13:05:15 -0600

    add Duplicate Zeros

--- LeetCode/Arrays_and_Strings/Duplicate_Zeros/index.js
+++ LeetCode/Arrays_and_Strings/Duplicate_Zeros/index.js
@@ -3,14 +3,17 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function(arr) {
-  let storage = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
-        storage.slice(i);
+    if (arr[i] !== 0) continue;
+
+    const saved = arr.slice(i);
+
+    for (let j = 0; j < saved.length - 1; j++) {
+      arr[i+j+1] = saved[j];
     }
+
+    i++;
   }
 };
-const array = [1,0,2,3,0,4,5,0]
-            //[1,0,0,2,3,0,0,4]
-duplicateZeros(array);
-console.log(array);
+// [1,0,2,3,0,4,5,0]  ---> [1,0,0,2,3,0,0,4]
+
